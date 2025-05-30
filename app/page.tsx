cd0d4b6ec820f9dffdbe116a048bb61bbaf0cad2
Lev <smolsky.lev@gmail.com>
2025-05-29 22:00:24 -0400

    created main page layout

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div style={{width: "50%", height: "80vh", margin: "10vh auto", display: "flex", flexDirection: "column", gap: '5%'}}>
+      <div style={{backgroundColor: "#e3e3e3", width: "100%", height: "10%", minHeight: "40px", borderRadius: '1vh'}}>
+        <h1 style={{fontWeight: 'bold', color: "#2d472e", fontSize: '5vh', textAlign: 'center'}}>
+          Recent Notes
+        </h1>
+      </div>
+      <div style={{backgroundColor: "#e3e3e3", width: "100%", height: "80%", minHeight: "40px", borderRadius: '1vh'}}>
+        
+      </div>
+    </div>
+  );
+}
